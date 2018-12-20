e1c0cffb5fb20c40e603dc494f4a8989c2e9b008
Jehy <fate@jehy.ru>
2018-12-20 19:31:32 +0300

    use red color in bg when error occures

--- bin/bin.js
+++ bin/bin.js
@@ -6,11 +6,16 @@
 
 const checkExact = require('../check_exact');
 
+const colors = {
+  red: ((...args)=>console.log(`\x1b[41m${args.join('')}\x1b[0m`)),
+};
+
 const path = process.argv.slice(2).join(' ').trim();
 const checked = checkExact.checkPackageByPath(path);
-console.log(checked.log);
 if (!checked.result)
 {
+  colors.red(checked.log);
   process.exit(1);
 }
+console.log(checked.log);
 process.exit(0);
